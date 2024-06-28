dfa0293d9ec851ef1d06ef2f5212f2d5111fb5fe
Egor <egor032324@mail.ru>
2024-06-28 15:46:52 +0300

    feat: implement a custom select

---
+++ src/ui/CustomSelect/CustomSelect.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+interface CustomSelectProps {
+  options: string[];
+  label: string;
+  onChange: (value: string[]) => void;
+  value: string[];
+}
+
+const CustomSelect: React.FC<CustomSelectProps> = ({
+  options,
+  label,
+  onChange,
+  value,
+}) => {
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const ITEM_HEIGHT = 28;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const handleChange = (event: SelectChangeEvent<typeof value>) => {
+    const {
+      target: { value },
+    } = event;
+    onChange(typeof value === "string" ? value.split(",") : value);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  return (
+    <FormControl sx={{ width: "100%", height: "36px" }}>
+      <InputLabel
+        id="custom-select-label"
+        sx={{
+          transform: isFocused
+            ? "translate(14px, -9px) scale(0.75)"
+            : "translate(14px, 6px) scale(1)",
+          transition: "transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms",
+        }}
+      >
+        {label}
+      </InputLabel>
+      <Select
+        labelId="custom-select-label"
+        id="custom-select"
+        multiple
+        value={value}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        input={<OutlinedInput label={label} />}
+        MenuProps={MenuProps}
+        sx={{ height: 36, backgroundColor: "var(--steel-gray---40)" }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default CustomSelect;
